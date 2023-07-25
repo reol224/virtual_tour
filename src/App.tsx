fafb8bb829d721a7ea0ec2a3110ee46df7409408
Iulia Danilov <tmdoppelganger@gmail.com>
2023-07-25 14:44:23 -0400

    Rotating Mercury

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import './App.css'
-import PlanetExplorer from "./components/PlanetExplorer";
+
+import {useState} from 'react';
+
+import mercuryPlanet from './assets/mercury.png';
 
 function App() {
+    const [count, setCount] = useState(0);
+
     return (
-        <div>
-            <h1>Planet Explorer</h1>
-            <PlanetExplorer />
-        </div>
+        <>
+            <div className="planet mercury">
+                <img src={mercuryPlanet} className="planet-img" alt="Mercury Planet"/>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on Mercury to learn more
+            </p>
+        </>
     );
 }
 
-export default App
+export default App;
+

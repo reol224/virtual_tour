423ce2486f7f24d81eae3b9954928ce7ec6fa0f2
Iulia Danilov <tmdoppelganger@gmail.com>
2023-07-25 15:05:41 -0400

    Fact card

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import './App.css'
-
-import {useState} from 'react';
-
+import { useState } from 'react';
+import './App.css';
 import mercuryPlanet from './assets/mercury.png';
+import mercuryFacts from './data/mercuryFacts.json';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [showFacts, setShowFacts] = useState(false);
+
+    const handlePlanetClick = () => {
+        setShowFacts(!showFacts);
+    };
 
     return (
         <>
-            <div className="planet mercury">
-                <img src={mercuryPlanet} className="planet-img" alt="Mercury Planet"/>
+            <div className={`planet mercury${showFacts ? " active" : ""}`} onClick={handlePlanetClick}>
+                <img src={mercuryPlanet} className="planet-img" alt="Mercury Planet" />
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on Mercury to learn more
-            </p>
+            {showFacts && (
+                <div className="card">
+                    <h2>Mercury Facts</h2>
+                    {mercuryFacts.map((fact, index) => (
+                        <div key={index}>
+                            <h3>{fact.title}</h3>
+                            <p>{fact.fact}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </>
     );
 }
 
 export default App;
-

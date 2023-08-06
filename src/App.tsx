b20b936fd9d0d7869b6287633b2c81f76817b340
Iulia Danilov <tmdoppelganger@gmail.com>
2023-08-06 16:15:24 -0400

    Earth

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import mercuryPlanet from './assets/mercury.png';
 import mercuryFacts from './data/mercuryFacts.json';
 import venusPlanet from './assets/venus.png';
 import venusFacts from './data/venusFacts.json';
+import earthPlanet from './assets/earth.png';
+import earthFacts from './data/earthFacts.json';
 // Add other imports for other planets
 
 const App: React.FC = () => {
@@ -20,6 +22,11 @@ const App: React.FC = () => {
             planetImg: venusPlanet,
             facts: venusFacts,
         },
+        {
+            name: 'Earth',
+            planetImg: earthPlanet,
+            facts: earthFacts
+        }
         // Add other planets data here
     ];
 
